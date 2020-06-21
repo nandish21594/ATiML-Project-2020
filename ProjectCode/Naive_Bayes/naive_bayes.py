3a1c082b474919ec2881c5d075cb1d1c48b64521
sujithns <sujit.nss@gmail.com>
2020-06-21 15:33:24 +0200

    [Genre Classification] Added code for naive bayes classifier with and without gridsearch

--- ProjectCode/Naive_Bayes/naive_bayes.py
+++ ProjectCode/Naive_Bayes/naive_bayes.py
@@ -34,14 +34,31 @@ from collections import OrderedDict
 import spacy
 from textstat.textstat import textstatistics, legacy_round
 from sklearn.preprocessing import StandardScaler
+#Using Min Max scaler for NAive bayes so as to bound the values between 0 and 1
+#Negative values won't work for probability computations
+from sklearn.preprocessing import MinMaxScaler
+#GridSearch for hyperparams tuning and classification_report for generating report after param tuning
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+
 import en_core_web_sm
 from sklearn import svm
+#Naive Bayes from SKLearn
+from sklearn.naive_bayes import MultinomialNB
 nlp = en_core_web_sm.load()
 
+#Supress warning in console
+import warnings
+warnings.filterwarnings("ignore")
 
-data = pd.read_csv(
-    "final_features_without_null_values.csv",
-    encoding="ISO-8859-1")
+#For giving relative path
+from pathlib import Path
+base_path = Path(__file__).parent
+file_path = (base_path / "../final_features_without_null_values.csv").resolve()
+
+data = pd.read_csv(file_path,encoding="ISO-8859-1")
+
+#data = pd.read_csv("final_features_without_null_values.csv",encoding="ISO-8859-1")
 
 # Split Train and Test Data
 y_true = data['class'].values
@@ -60,19 +77,18 @@ print("\ntraining distribution:\n\n", train_class_distribution)
 # print("\ncv distribution: \n\n", cv_class_distribution)
 print("\ntest distribution: \n\n", test_class_distribution)
 
-scaler = StandardScaler()
+scaler = MinMaxScaler()
 train_df = scaler.fit_transform(x_train)
 test_df = scaler.fit_transform(test_df)
 
 
-clf = svm.SVC()
+# Alpha=Smoothening parameter(0 for no smoothening)
+# Other hyperparams for naive bayes fit_prior & class prior
+# If we know class prior set fit prior to yes and define class prior, Now since we don't know we are going with uniform prior for classes
+clf = MultinomialNB(alpha=.45)
 clf.fit(train_df, y_train)
 pred = clf.predict(test_df)
-# clf = MultinomialNB(alpha=.45)
-#
-# clf.fit(train_df, y_train)
-# pred = clf.predict(test_df)
-#
+
 print('*******************F1 Score and Accuracy*****************************')
 print('F1 Score: ', metrics.f1_score(y_test, pred, average='macro'))
 print('Accuracy: ', metrics.accuracy_score(y_test, pred))
