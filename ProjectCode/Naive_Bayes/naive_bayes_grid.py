f0b38ebc640f60a328c2e111416be54823230e87
sujithns <sujit.nss@gmail.com>
2020-06-22 13:05:02 +0200

    [Book Genre Classification]Minor code fix for fitting test train data into models, added word2vec model for comparison

--- ProjectCode/Naive_Bayes/naive_bayes_grid.py
+++ ProjectCode/Naive_Bayes/naive_bayes_grid.py
@@ -58,20 +58,21 @@ file_path = (base_path / "../final_features_without_null_values.csv").resolve()
 
 data = pd.read_csv(file_path,encoding="ISO-8859-1")
 
-#data = pd.read_csv("final_features_without_null_values.csv",encoding="ISO-8859-1")
-
 # Split Train and Test Data
 y_true = data['class'].values
-x_train, test_df, y_train, y_test = train_test_split(data, y_true, stratify = y_true, test_size = 0.2)
+X  = data.iloc[:,:-1]
+x_train, test_df, y_train, y_test = train_test_split(X, y_true, stratify = y_true, test_size = 0.2)
+df_y_train = pd.DataFrame(y_train, columns=['class'])
+df_y_test = pd.DataFrame(y_test, columns=['class'])
 # train_df, cv_df, y_train, y_cv = train_test_split(x_train, y_train, stratify = y_train, test_size = 0.2)
 
 print("Number of samples in training data :", x_train.shape[0])
 # print("Number of samples in validation data :", cv_df.shape[0])
 print("Number of samples in test data :", test_df.shape[0])
 
-train_class_distribution = x_train['class'].value_counts().sort_index()
+train_class_distribution = df_y_train['class'].value_counts().sort_index()
 # cv_class_distribution = cv_df['class'].value_counts().sort_index()
-test_class_distribution = test_df['class'].value_counts().sort_index()
+test_class_distribution = df_y_test['class'].value_counts().sort_index()
 
 print("\ntraining distribution:\n\n", train_class_distribution)
 # print("\ncv distribution: \n\n", cv_class_distribution)
